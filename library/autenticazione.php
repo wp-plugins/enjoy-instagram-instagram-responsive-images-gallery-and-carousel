6e38d95fd2306d8ee5531f784e3c0849248576a7
mediabeta <mediabeta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-06 09:23:58 +0000

    insert icon and modified some text errors
    git-svn-id: https://plugins.svn.wordpress.org/enjoy-instagram-instagram-responsive-images-gallery-and-carousel/trunk@1106669 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- library/autenticazione.php
+++ library/autenticazione.php
@@ -1,4 +1,4 @@
-<p style="font-size:14px;">Thank you for you choise! <strong>Enjoy Instagram - Responsive gallery</strong> is a plugin lovingly developed for you by <a href="http://www.mediabeta.com" target="_blank"> Mediabeta</a>.</p>
+<p style="font-size:14px;">Thank you for you choice! <strong>Enjoy Instagram - Responsive gallery</strong> is a plugin lovingly developed for you by <a href="http://www.mediabeta.com" target="_blank"> Mediabeta</a>.</p>
 
 <p style="font-size:14px;">By using this plugin, you are agreeing to the <a href="http://instagram.com/about/legal/terms/api/" target="_blank">Instagram API Terms of Use</a>.</p>
 
